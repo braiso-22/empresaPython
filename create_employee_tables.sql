a9a87a3873bf3aab5d32f665e60022422776cdc0
Brais <81094589+braiso-22@users.noreply.github.com>
2021-11-04 14:25:00 +0100

    cambio de base de datos

---
+++ create_employee_tables.sql
@@ -0,0 +1,18 @@
+use mysql;
+
+create database factory;
+use factory;
+
+CREATE TABLE Departments (
+  Code INTEGER PRIMARY KEY,
+  Name varchar(255) NOT NULL ,
+  Budget decimal NOT NULL 
+);
+
+CREATE TABLE Employees (
+  SSN INTEGER PRIMARY KEY,
+  Name varchar(255) NOT NULL ,
+  LastName varchar(255) NOT NULL ,
+  Department INTEGER NOT NULL , 
+  foreign key (department) references Departments(Code) 
+) ENGINE=INNODB;
